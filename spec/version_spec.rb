03b799589755e66d4d659750b212b6a3ff9aa4ca
Scott Taylor <scott@railsnewbie.com>
2009-09-05 01:17:54 -0400

    Fix version spec

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/spec_helper")
 module MixMaster
   describe VERSION do
     it "should be at VERSION 0.0.2" do
-      MixMaster::Version::STRING.should == "0.0.2"
+      MixMaster::Version::STRING.should == "0.0.3"
     end
   end
 end
